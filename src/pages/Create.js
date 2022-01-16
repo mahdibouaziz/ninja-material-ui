7dd0cd882ff5e0e7671695bc02c055a8893e6eb9
mahdi <mahdibouaziz@hotmail.fr>
2022-01-16 19:46:24 +0100

    Typographie, Buttons Icons and Custom theme

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Button, Typography, Container } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: "red",
+    fontSize: 60,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        endIcon={<SendIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
